eaa12c8fb37652e43094b5cd1c920f27cc03a723
lehoatptit <90516963+lehoatptit@users.noreply.github.com>
2024-11-18 14:05:29 +0700

    Update redis-entrypoint.sh

--- redis-entrypoint.sh
+++ redis-entrypoint.sh
@@ -3,6 +3,6 @@
 #khai bao bien duong dan file configure redis trong image da build
 CONFIG_PATH="/etc/redis/conf/redis.conf"
 
-# khoi dong image
+# khoi dong service redis
 exec redis-server $CONFIG_PATH
 
